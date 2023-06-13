1341de4d7bfdc4d41cced6cf8728301915291ad7
mohamedumar07 <bilalumar56789@gmail.com>
2023-06-13 23:32:13 +0530

    First commit

---
+++ taxi_fare_calculator/src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import Taxi from './Taxi1';
+
+class App extends Component{
+    render(){
+        return(
+            <div className='css1'>
+                <Taxi />
+            </div>
+        )
+    }
+}
+export default App;
